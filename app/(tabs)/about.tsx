bbcb729df5418b9fa78c52cedffa39472225a4ca
jude7733 <judeputhussery360@gmail.com>
2025-08-30 19:32:44 +0530

    added credits

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,11 +3,23 @@ import { Image, ImageBackground } from 'expo-image';
 import { Text } from '~/components/ui/text';
 import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
 import { Button } from '~/components/ui/button';
-import { adLogos, contacts } from '~/lib/constants';
+import { contacts } from '~/lib/constants';
+import { Github } from 'lucide-react-native';
 
 export default function About() {
   const imageUrl =
     'https://images.unsplash.com/photo-1642658228905-3672d6c64d9b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
+  const developers = [
+    {
+      name: 'Jude Saju',
+      socialUrl: 'https://github.com/jude7733',
+    },
+    {
+      name: 'Jeffin Pappachan',
+      socialUrl: 'https://github.com/JeffinPappachan',
+    },
+  ];
 
   const openPhone = (phone: string) => {
     Linking.openURL(`tel:${phone}`);
@@ -15,6 +27,10 @@ export default function About() {
 
   const openEmail = () => {
     Linking.openURL('mailto:jude7733@protonmail.com?subject=Bug Report');
+  };
+
+  const openUrl = (url: string) => {
+    Linking.openURL(url);
   };
 
   return (
@@ -26,10 +42,10 @@ export default function About() {
       <ScrollView
         className="flex-1 w-full"
         contentContainerStyle={{
-          paddingBottom: 40,
+          paddingBottom: 20,
           alignItems: 'center',
-          gap: 20,
-          paddingHorizontal: 16,
+          gap: 12,
+          paddingHorizontal: 12,
           paddingVertical: 24,
         }}
       >
@@ -37,7 +53,7 @@ export default function About() {
         <Card className="p-8 py-10 gap-4 rounded-xl max-w-4xl flex-row flex-wrap items-center justify-center">
           <CardHeader>
             <CardTitle>
-              <Text className="text-4xl text-center">About us</Text>
+              <Text className="text-4xl text-center">About Us</Text>
             </CardTitle>
           </CardHeader>
           <CardContent>
@@ -73,9 +89,31 @@ export default function About() {
           </Text>
         </Card>
 
+        {/*  Developers Section  */}
+        <Card className="rounded-xl max-w-4xl w-full">
+          <CardHeader className="items-center">
+            <CardTitle>
+              <Text className="text-2xl font-bold mb-4 text-center">Meet the Developers</Text>
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="gap-y-2">
+            {developers.map((dev, index) => (
+              <View
+                key={index}
+                className="flex-row items-center justify-between w-full"
+              >
+                <Text className="text-lg">{dev.name}</Text>
+                <Button size="icon" variant="default" onPress={() => openUrl(dev.socialUrl)}>
+                  <Github className="inline-block mr-2" color="white" size={16} />
+                </Button>
+              </View>
+            ))}
+          </CardContent>
+        </Card>
+
         {/* Contact Us Section */}
         <View className="rounded-xl max-w-4xl w-full p-6 bg-white dark:bg-gray-900 shadow-md">
-          <Text className="text-3xl font-bold mb-6 text-center">Contact Us</Text>
+          <Text className="text-2xl font-bold mb-6 text-center">Contact Us</Text>
           {contacts.map((contact, index) => (
             <Pressable
               key={index}
